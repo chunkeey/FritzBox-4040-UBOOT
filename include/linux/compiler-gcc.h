f89b3260ab21f87a10291a4413ad8cc372d580f5
Christian Lamparter <chunkeey@gmail.com>
2018-07-02 21:24:29 +0200

    uboot-fritz4040: force gcc >= 5.0
    Signed-off-by: Christian Lamparter <chunkeey@gmail.com>

--- include/linux/compiler-gcc.h
+++ include/linux/compiler-gcc.h
@@ -81,7 +81,8 @@
 #define __maybe_unused			__attribute__((unused))
 #define __always_unused			__attribute__((unused))
 
-#define __gcc_header(x) #x
-#define _gcc_header(x) __gcc_header(linux/compiler-gcc##x.h)
-#define gcc_header(x) _gcc_header(x)
-#include gcc_header(__GNUC__)
+#if __GNUC__ < 5
+#error "GCC version must be 5.x+"
+#endif
+
+#include "linux/compiler-gcc5.h"
