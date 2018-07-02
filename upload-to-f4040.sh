3633143f83b8195f2a68a66d093343bf6990d290
Christian Lamparter <chunkeey@gmail.com>
2018-07-02 21:39:11 +0200

    uboot-fritz4040: improve upload-to-f4040.sh
    The script has been updated to take the first parameter as
    the file it is supposed to flash, otherwise it defaults to
    the previous "uboot-fritz4040.bin". Furthermore the error
    messages have been improved and ftp will now dump some
    "progress information" to the user's console.
    
    Signed-off-by: Christian Lamparter <chunkeey@gmail.com>

--- upload-to-f4040.sh
+++ upload-to-f4040.sh
@@ -1,34 +1,37 @@
 #!/bin/sh
 
 die() {
-	echo $1
+	(>&2 echo $@)
 	exit 1
 }
 
 FRITZBOX='192.168.178.1'
 USER='adam2'
 PASSWD='adam2'
-FILE='uboot-fritz4040.bin'
+FILE=${1-'uboot-fritz4040.bin'}
 
-ping -q -4 -w 1 -c 1 "$FRITZBOX" &> /dev/null  || die "Fritzbox at $FRITZBOX is not reachable. aborting"
+[ -r "$FILE" ] || die -e "u-boot image '$FILE' is not readable.\naborting."
+
+ping -q -4 -w 1 -c 1 "$FRITZBOX" &> /dev/null || die -e "Fritzbox at $FRITZBOX is not reachable (yet). \nMake sure to use the yellow LAN ports!.\naborting."
 
 cat << EOS
 
-This will take ages (2 minutes)! If you want a
-status bar: Attach a serial to the device and look there.
+This will take ages (2 minutes+)! If you want a status bar:
+Please attach a serial to the device and look there.
 
-Note: If this fails, you need to use AVM\' recover
-utility to get your box back. However this step
-should not damage your box....
+Note:This script does not terminate on its own.. :/
+Once you see a message like this appear on the console:
+150 Flash check 0x...
 
-Also it doesn't terminate.. :/
+you can power-cycle your fritzbox and force-quit this script.
 
-(Note: Make sure that you connected the router on
-the yellow LAN ports and not the blue WAN).
+Note2: If this fails, you can just replug the power cable and
+give it one more try. To go back to AVM's stock firmware you
+have to download and run AVM's recover utility.
 
 EOS
 
-ftp -n -4 "$FRITZBOX" << END_SCRIPT
+ftp -n -v -4 "$FRITZBOX" << END_SCRIPT
 quote USER $USER
 quote PASS $PASSWD
 quote MEDIA FLSH
