10f88d601ac8a93a6de2884ecfb59b094f1fb591
David Bauer <mail@david-bauer.net>
2019-01-31 00:20:04 +0100

    fritzcreator: generalize build

--- fritz/fritzcreator.sh
+++ fritz/fritzcreator.sh
@@ -6,14 +6,21 @@
 #
 #
 
+BOARDNAME=$1
+
+if [ -z "$BOARDNAME" ];
+then
+	echo "Usage: fritzcreator.sh <BOARDNAME>"
+	exit 1
+fi
 UBOOT_BIN="u-boot.bin"
-UBOOT_FRITZ4040="uboot-fritz4040.bin"
-FRITZ_DTS="fritz/fritz4040.dts"
-FRITZ_DTB="fritz4040.dtb"
+UBOOT_FRITZ="uboot-${BOARDNAME}.bin"
+FRITZ_DTS="fritz/${BOARDNAME}.dts"
+FRITZ_DTB="${BOARDNAME}.dtb"
 UBOOT_LOADADDR=0x841FFFF8
 DTC="${DTC:-dtc}"
 
-rm -f "$UBOOT_FRITZ4040"
+rm -f "$UBOOT_FRITZ"
 
 # Can you guys *PLEASE* switch to FIT?!
 
@@ -38,25 +45,25 @@ rm -f "$UBOOT_FRITZ4040"
 #
 # Note2: The address space switches between 0x84...... in EVA to 0xc4......
 
-printf "\x00\x00\x00\xea\xf8\x01\x33\xc4" > $UBOOT_FRITZ4040
+printf "\x00\x00\x00\xea\xf8\x01\x33\xc4" > $UBOOT_FRITZ
 
 # Append u-boot
 
-cat "$UBOOT_BIN" >> "$UBOOT_FRITZ4040"
+cat "$UBOOT_BIN" >> "$UBOOT_FRITZ"
 
 # Pad file to 1M
-dd if="$UBOOT_FRITZ4040" of="$UBOOT_FRITZ4040.new" bs=1024k count=1 conv=sync
-mv "$UBOOT_FRITZ4040.new" "$UBOOT_FRITZ4040"
+dd if="$UBOOT_FRITZ" of="$UBOOT_FRITZ.new" bs=1024k count=1 conv=sync
+mv "$UBOOT_FRITZ.new" "$UBOOT_FRITZ"
 
 # Compile DTS
 $DTC "$FRITZ_DTS" -o "$FRITZ_DTB" --space 49152
 
 # Append the compiled DTBs
-cat "$FRITZ_DTB" "$FRITZ_DTB" "$FRITZ_DTB" "$FRITZ_DTB" >> $UBOOT_FRITZ4040
+cat "$FRITZ_DTB" "$FRITZ_DTB" "$FRITZ_DTB" "$FRITZ_DTB" >> $UBOOT_FRITZ
 rm -f "$FRITZ_DTB"
 
 # Add 512 bytes of pad area
-printf "%0.s\0" {1..512} >> $UBOOT_FRITZ4040
+printf "%0.s\0" {1..512} >> $UBOOT_FRITZ
 
 # This table links to the individual DTBs for every HWSubRevision.
 # A table entry consists of two 32-bit words.
@@ -72,51 +79,51 @@ printf "%0.s\0" {1..512} >> $UBOOT_FRITZ4040
 # ...
 # 0xC41FFFF8 + 0x100000 + 3 * DTB_SIZE = 0xC4908000
 # (And the last entry is left empty)
-( cat "$UBOOT_FRITZ4040";                              	\
+( cat "$UBOOT_FRITZ";                              	\
           printf "\xfc\x01\x33\xc4";                    \
           printf "\x01\x00\x00\x00\xF8\xFF\x2F\xc4";    \
           printf "\x02\x00\x00\x00\xF8\xFF\x2F\xc4";    \
           printf "\x05\x00\x00\x00\xF8\xBF\x30\xc4";    \
           printf "\x06\x00\x00\x00\xF8\x7F\x31\xc4";    \
           printf "\x07\x00\x00\x00\xF8\x3F\x32\xc4";    \
-          printf "\x06\x01\x00\x00\x00\x00\x00\x00" ) > $UBOOT_FRITZ4040.new
+          printf "\x06\x01\x00\x00\x00\x00\x00\x00" ) > $UBOOT_FRITZ.new
 
-mv "$UBOOT_FRITZ4040.new" "$UBOOT_FRITZ4040"
+mv "$UBOOT_FRITZ.new" "$UBOOT_FRITZ"
 
 
 # Add 64k bytes of pad area
-printf "%0.s\0" {1..65536} >> $UBOOT_FRITZ4040
+printf "%0.s\0" {1..65536} >> $UBOOT_FRITZ
 
 # Pack it with lzma
-fritz/lzma e "$UBOOT_FRITZ4040" -lc1 -lp2 -pb2 "$UBOOT_FRITZ4040.new"
+fritz/lzma e "$UBOOT_FRITZ" -lc1 -lp2 -pb2 "$UBOOT_FRITZ.new"
 
 # Make it a EVA image
-fritz/lzma2eva $UBOOT_LOADADDR $UBOOT_LOADADDR "$UBOOT_FRITZ4040.new" "$UBOOT_FRITZ4040"
+fritz/lzma2eva $UBOOT_LOADADDR $UBOOT_LOADADDR "$UBOOT_FRITZ.new" "$UBOOT_FRITZ"
 
 # The bootloader seems to add a TI checksum signature (8 Bytes) as part of the
 # "check mtd1" command in the FTP prompt. To make this easier we add spacer here.
-(cat "$UBOOT_FRITZ4040"; printf "\xff\xff\xff\xff\xff\xff\xff\xff" ) > $UBOOT_FRITZ4040.new
+(cat "$UBOOT_FRITZ"; printf "\xff\xff\xff\xff\xff\xff\xff\xff" ) > $UBOOT_FRITZ.new
 
 # The next bit. The hshqs partition should be aligned to 0x100
-let size=$(stat -c%s "$UBOOT_FRITZ4040")
+let size=$(stat -c%s "$UBOOT_FRITZ")
 let "pad = 256 - ( $size % 256) % 256"
-( printf "%0.s\377" {1..256} | dd conv=sync bs=$pad count=1 ) > $UBOOT_FRITZ4040.pad
+( printf "%0.s\377" {1..256} | dd conv=sync bs=$pad count=1 ) > $UBOOT_FRITZ.pad
 
-cat "$UBOOT_FRITZ4040" "$UBOOT_FRITZ4040.pad" > $UBOOT_FRITZ4040.new
+cat "$UBOOT_FRITZ" "$UBOOT_FRITZ.pad" > $UBOOT_FRITZ.new
 
-mv "$UBOOT_FRITZ4040.new" "$UBOOT_FRITZ4040"
+mv "$UBOOT_FRITZ.new" "$UBOOT_FRITZ"
 
-rm -f "$UBOOT_FRITZ4040.pad"
+rm -f "$UBOOT_FRITZ.pad"
 
 # Apparently, EVA checks for the SquashFS filesystem MAGIC too. Likely for the rootfs
 # entry.
-(cat "$UBOOT_FRITZ4040"; echo "hsqs"; printf "%0.s\0" {1..124} ) > $UBOOT_FRITZ4040.new
+(cat "$UBOOT_FRITZ"; echo "hsqs"; printf "%0.s\0" {1..124} ) > $UBOOT_FRITZ.new
 
 # Make it so that this fits into 512k (Note: we have to add 8 Bytes for the final checksum
 # so 524280 is 512k - 8.
-dd if="$UBOOT_FRITZ4040.new" of="$UBOOT_FRITZ4040" conv=sync bs=524280 count=1
-rm "$UBOOT_FRITZ4040.new"
+dd if="$UBOOT_FRITZ.new" of="$UBOOT_FRITZ" conv=sync bs=524280 count=1
+rm "$UBOOT_FRITZ.new"
 
-fritz/tichksum -a "$UBOOT_FRITZ4040"
+fritz/tichksum -a "$UBOOT_FRITZ"
 
 echo "Done."
--- make-uboot.sh
+++ make-uboot.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 OPENWRT_OR_LEDE_TOOLCHAIN_FOR_ARM7=/mnt/build/lede/ac58u/staging_dir/toolchain-arm_cortex-a7+neon-vfpv4_gcc-8.1.0_musl_eabi/bin/
+BOARDNAME=$1
 
 die() {
 	echo Error: $1
@@ -20,6 +21,8 @@ EOF
 	exit 1
 }
 
+[ -z "$BOARDNAME" ] && die "Usage: make-uboot <BOARDNAME>"
+
 [ -e "$OPENWRT_OR_LEDE_TOOLCHAIN_FOR_ARM7" ] || no_toolchain
 
 export PATH="$PATH:$OPENWRT_OR_LEDE_TOOLCHAIN_FOR_ARM7"
@@ -29,12 +32,12 @@ arm-openwrt-linux-ld -v || no_toolchain
 
 make clean || die "Can't clean old cruft"
 
-USE_PRIVATE_LIBGCC=yes make fritz4040 || die "Failed to set build target"
+USE_PRIVATE_LIBGCC=yes make $BOARDNAME || die "Failed to set build target"
 
 USE_PRIVATE_LIBGCC=yes make || die "Failure during u-boot build"
 
 [ -e u-boot.bin ] || die "Build succeeded. But u-boot.bin wasn't created"
 
-fritz/fritzcreator.sh || die "Failure during Fritzing"
+fritz/fritzcreator.sh $BOARDNAME || die "Failure during Fritzing"
 
-[ -e uboot-fritz4040.bin ] || die "No idea, but the uboot-fritz4040.bin wasn't created."
+[ -e uboot-${BOARDNAME}.bin ] || die "No idea, but the uboot-${BOARDNAME}.bin wasn't created."
